994c63a939d199070a9b6efa1e290c9e628809a3
hinauni <5h.3.ybdgt@gmail.com>
2022-05-27 17:12:59 +0900

    初回コミット

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i + 1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示する')
+if button:
+    right_column.write('ここは右カラム')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+# option = st.text_input('あなたの趣味を教えてください')
+# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+# 'あなたの趣味:', option
+# 'コンディション:', condition
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('sample.jpg')
+#     st.image(img, caption='Marteez', use_column_width=True)
+
